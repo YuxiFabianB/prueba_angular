8d0910fff9b44a7764dd2dbe71ebbfff65b8af24
Dayro Longas <dgiovannyl@gmail.com>
2018-06-14 09:17:02 -0500

    Add behavior to not-found-page to redirect after 3 seconds to Login page.

--- src/app/shared/components/not-found/not-found.component.ts
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-not-found',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  secondsToRedirect: number;
+
+  constructor(
+    private auth: AuthService
+  ) {
+    debugger;
+    this.secondsToRedirect = 3;
+  }
 
   ngOnInit() {
+    this.counter();
+  }
+
+  counter() {
+    setTimeout(() => {
+      if (this.secondsToRedirect <= 0) {
+        this.auth.logout();
+        this.auth.login();
+      } else {
+        this.secondsToRedirect--;
+        this.counter();
+      }
+    }, 1000);
   }
 
 }
